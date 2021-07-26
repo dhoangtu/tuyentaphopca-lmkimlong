02a67f2be3a8f33720c5ccf42136eb128b8742d9
dhoangtu <dhoangtu@gmail.com>
2021-07-26 15:19:08 +0700

    tuyen tap hop ca - lm kim long

---
+++ sach/in-sach.bat
@@ -0,0 +1,26 @@
+
+
+@ECHO ON
+rem setlocal enabledelayedexpansion
+
+set LILYPOND="C:\Program Files (x86)\LilyPond-2.22.1\usr\bin\lilypond.exe"
+
+set GEN=".\pdf-generated"
+mkdir %GEN%
+
+for %%f in (D:\01.lilypond\01.github\tuyentaphopca-lmkimlong\nhac\*.ly) do (
+  set /p val=<%%f
+  echo "fullname: %%f"
+  echo "name: %%~nf"
+  
+  %LILYPOND% --output="%GEN%\%%~nf" -dno-point-and-click --pdf "%%f"
+)
+
+pdftk %GEN%\*.pdf cat output nhac.pdf
+
+pdflatex so-trang-chan-le.tex
+
+pdftk blank-a4.pdf so-trang-chan-le.pdf cat output tuyentaphopca-lmkimlong.pdf
+
+del /s /f /q %GEN% nhac.pdf *.aux *.log so-trang-chan-le.pdf
+rmdir /s/q %GEN%
